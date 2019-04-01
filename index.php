2ae9939023bd0bbd25da8d58112151bbd85055ef
taudau1 <37015042+taudau1@users.noreply.github.com>
2019-04-01 17:02:12 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
-<?php
-include("src/functions.php");
-function calculateHomeWorkSum(...$numbers){};
-echo $int;
-calculateHomeWorkSum(3, 2.2, '1');
+<?php 
+require("vendor/autoload.php");
+echo calculateHomeWorkSum(3, 2.2, '1') . "<br>";
+echo  Nfq\Akademija\Not_Typed\calculateHomeWorkSum(3, 2.2, '1') . "<br>";
+echo  Nfq\Akademija\Soft\calculateHomeWorkSum(3, 2.2, '1') . "<br>";
+echo  Nfq\Akademija\Strict\calculateHomeWorkSum(3, 2.2, '1');
