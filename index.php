0c5d9fcdcdc2dfc9c3afcd3bc917bd79080ff331
taudau1 <37015042+taudau1@users.noreply.github.com>
2019-04-01 10:39:35 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+include("src/functions.php");
+function calculateHomeWorkSum(...$numbers){};
+echo $int;
+calculateHomeWorkSum(3, 2.2, '1');
---
+++ src/functions_strict.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tautvydas
+ * Date: 22/03/2019
+ * Time: 11:02
+ */
